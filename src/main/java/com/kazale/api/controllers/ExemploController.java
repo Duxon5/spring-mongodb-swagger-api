17f68ee2179b94c9cff34208fea5a8903ba4b8a8
Sidnei G. de Azevedo Júnior <sigajunior@hotmail.com>
2022-10-05 22:37:25 -0300

    Envio inicial dos arquivos

---
+++ src/main/java/com/kazale/api/controllers/ExemploController.java
@@ -0,0 +1,24 @@
+package com.kazale.api.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exemplo")
+public class ExemploController {
+	
+	@GetMapping(value = "/exemploOld/{nome}")
+	public String exemploOld(@PathVariable("nome") String nome) {
+		 return "Olá " + nome;
+	}
+	
+	@GetMapping(value = "/{nome}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public String exemplo(@PathVariable("nome") String nome) {
+		return "Olá " + nome;
+	}
+
+}
